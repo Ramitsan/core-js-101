07ac9a9edb1b3dfc1c5c6b605f11d0bbf54f623d
Ramitsan <ramitsan@gmail.com>
2022-12-05 22:16:36 +0300

    feat: add solutions of 04-date-tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,10 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  const checkedDay = new Date(year, 1, 29).getDate();
+  return (checkedDay === 29);
 }
 
 
@@ -73,10 +75,32 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
-}
 
+function timeSpanToString(startDate, endDate) {
+  const period = Math.abs(endDate - startDate);
+  const millisecInHour = 3600000;
+  const millisecInMinute = 60000;
+  const millisecInSecond = 1000;
+
+  const hours = Math.floor(period / millisecInHour).toString();
+  const minutes = Math.floor((period - hours * millisecInHour) / millisecInMinute).toString();
+
+  const seconds = Math.floor((period - hours * millisecInHour - minutes * millisecInMinute)
+    / millisecInSecond).toString();
+
+  const milliseconds = Math.floor(period - (hours * millisecInHour)
+    - (minutes * millisecInMinute) - (seconds * millisecInSecond)).toString();
+
+  const hoursResult = `${hours > 9 ? hours : `0${hours}`}`;
+  const minutesResult = `${minutes > 9 ? minutes : `0${minutes}`}`;
+  const secondsResult = `${seconds > 9 ? seconds : `0${seconds}`}`;
+
+  let millisecondsResult = milliseconds;
+  if (milliseconds < 10) millisecondsResult = `00${milliseconds}`;
+  if (milliseconds > 10 && milliseconds < 100) millisecondsResult = `0${milliseconds}`;
+
+  return `${hoursResult}:${minutesResult}:${secondsResult}.${millisecondsResult}`;
+}
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
